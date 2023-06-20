aeb8c7539d110616a8274db4c499a16846a13956
henrybarrera <barrera.h@northeastern.edu>
2023-06-20 16:10:35 -0400

    Browse now opens Finder and fills in the textbox with absolute path

--- src/main/java/cs3500/pa05/controller/InitialController.java
+++ src/main/java/cs3500/pa05/controller/InitialController.java
@@ -3,11 +3,49 @@ package cs3500.pa05.controller;
 import cs3500.pa05.model.Bujo;
 import cs3500.pa05.view.InitialView;
 
+import java.io.File;
 import java.io.IOException;
+import javafx.stage.FileChooser;
 
 /**
  * Controls the initial view which asks the user for a path
  */
+//public class InitialController {
+//  private BujoSerializer serializer;
+//  private Bujo bujo;
+//  private InitialView view;
+//
+//  public InitialController(InitialView view, BujoSerializer serializer) {
+//    this.view = view;
+//    this.serializer = serializer;
+//    this.initViewEvents();
+//  }
+//
+//  void initViewEvents() {
+//    /*
+//    view.getBrowseButton().setOnAction(e -> {
+//      String path = view.getFilePathField().getText();
+//      loadBujo(path);
+//    });
+//     */
+//
+//    view.getFilePathField().setOnAction(e -> {
+//      String path = view.getFilePathField().getText();
+//      loadBujo(path);
+//    });
+//  }
+//
+//  private void loadBujo(String path) {
+//    try {
+//      bujo = serializer.read(path);
+//      view.displayWeek(bujo.toString());
+//    } catch (IOException e) {
+//      view.displayError("Error opening file",
+//          "An error occurred while opening the file.");
+//    }
+//  }
+//}
+
 public class InitialController {
   private BujoSerializer serializer;
   private Bujo bujo;
@@ -20,21 +58,25 @@ public class InitialController {
   }
 
   void initViewEvents() {
-    view.getBrowseButton().setOnAction(e -> {
-      String path = view.getFilePathField().getText();
-      loadBujo(path);
-    });
-
-    view.getFilePathField().setOnAction(e -> {
-      String path = view.getFilePathField().getText();
-      loadBujo(path);
-    });
+    view.getBrowseButton().setOnAction(e -> browseFile());
+    view.getFilePathField().setOnAction(e -> loadBujo());
   }
 
-  private void loadBujo(String path) {
+  private void browseFile() {
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Open Resource File");
+    File selectedFile = fileChooser.showOpenDialog(view.getScene().getWindow());
+    if (selectedFile != null) {
+      view.getFilePathField().setText(selectedFile.getAbsolutePath());
+      loadBujo();
+    }
+  }
+
+  private void loadBujo() {
+    String filePath = view.getFilePathField().getText();
     try {
-      bujo = serializer.read(path);
-      view.displayWeek(bujo.toString());
+      bujo = serializer.read(filePath);
+      //view.displayWeek(bujo.toString());
     } catch (IOException e) {
       view.displayError("Error opening file",
           "An error occurred while opening the file.");
--- src/main/java/cs3500/pa05/view/InitialView.java
+++ src/main/java/cs3500/pa05/view/InitialView.java
@@ -1,5 +1,7 @@
 package cs3500.pa05.view;
 
+import cs3500.pa05.controller.BujoSerializer;
+import cs3500.pa05.model.Bujo;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,22 +18,85 @@ import javafx.stage.Stage;
 /**
  * The initial view which prompts the user for a path
  */
+//public class InitialView extends BorderPane {
+//  private final TextField filePathField;
+//  private final Button browseButton;
+//
+//  public InitialView(Stage primaryStage) {
+//    Label filePathLabel = new Label("Enter a File Path:");
+//    filePathField = new TextField();
+//    browseButton = new Button("Browse");
+//    browseButton.setOnAction(e -> {
+//      browseFile(primaryStage);
+//      System.out.println("aaa");
+//    });
+//
+//    setTop(filePathLabel);
+//    setCenter(filePathField);
+//    setRight(browseButton);
+//    setPadding(new Insets(10));
+//
+//    //filePathField.setOnAction(e -> openBujoFile(filePathField.getText()));
+//  }
+//
+//  public Button getBrowseButton() {
+//    return browseButton;
+//  }
+//
+//  public TextField getFilePathField() {
+//    return filePathField;
+//  }
+//
+//  private void browseFile(Stage primaryStage) {
+//    FileChooser fileChooser = new FileChooser();
+//    //fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("BuJo Files", "*.bujo"));
+//    fileChooser.setTitle("Open Resource File");
+//    fileChooser.showOpenDialog(primaryStage);
+//    System.out.println("Hey");
+////    File selectedFile = fileChooser.showOpenDialog(primaryStage);
+////    if (selectedFile != null) {
+////      filePathField.setText(selectedFile.getAbsolutePath());
+////      openBujoFile(selectedFile.getAbsolutePath());
+////    }
+//  }
+//
+//  private void openBujoFile(String filePath) {
+//    try {
+//      //String fileContent = new String(Files.readAllBytes(Paths.get(filePath)));
+//      Bujo bujo = new BujoSerializer().read(filePath);
+//      JournalView journal = new JournalView(bujo.getWeek());
+//    } catch (IOException e) {
+//      displayError("Error opening file", "An error occurred while opening the file.");
+//    }
+//  }
+//
+//  public void displayWeek(String fileContent) {
+//    System.out.println("File contents:");
+//    System.out.println(fileContent);
+//  }
+//
+//  public void displayError(String title, String message) {
+//    Alert alert = new Alert(Alert.AlertType.ERROR);
+//    alert.setTitle(title);
+//    alert.setHeaderText(null);
+//    alert.setContentText(message);
+//    alert.showAndWait();
+//  }
+//}
+
 public class InitialView extends BorderPane {
   private final TextField filePathField;
   private final Button browseButton;
 
   public InitialView(Stage primaryStage) {
-    Label filePathLabel = new Label("File Path:");
+    Label filePathLabel = new Label("Enter a File Path:");
     filePathField = new TextField();
     browseButton = new Button("Browse");
-    browseButton.setOnAction(e -> browseFile(primaryStage, filePathField));
 
     setTop(filePathLabel);
     setCenter(filePathField);
     setRight(browseButton);
     setPadding(new Insets(10));
-
-    filePathField.setOnAction(e -> openBujoFile(filePathField.getText()));
   }
 
   public Button getBrowseButton() {
@@ -40,31 +105,6 @@ public class InitialView extends BorderPane {
 
   public TextField getFilePathField() {
     return filePathField;
-  }
-
-  private void browseFile(Stage primaryStage, TextField filePathField) {
-    FileChooser fileChooser = new FileChooser();
-    fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("BuJo Files",
-        "*.bujo"));
-    File selectedFile = fileChooser.showOpenDialog(primaryStage);
-    if (selectedFile != null) {
-      filePathField.setText(selectedFile.getAbsolutePath());
-      openBujoFile(selectedFile.getAbsolutePath());
-    }
-  }
-
-  private void openBujoFile(String filePath) {
-    try {
-      String fileContent = new String(Files.readAllBytes(Paths.get(filePath)));
-      displayWeek(fileContent);
-    } catch (IOException e) {
-      displayError("Error opening file", "An error occurred while opening the file.");
-    }
-  }
-
-  public void displayWeek(String fileContent) {
-    System.out.println("File contents:");
-    System.out.println(fileContent);
   }
 
   public void displayError(String title, String message) {
