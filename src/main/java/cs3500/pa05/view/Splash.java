26dddd7d59dc42d971c09936c77c3739eb79281a
anyueow <anyushah@gmail.com>
2023-06-22 08:46:13 -0400

    updated Splash.java

--- src/main/java/cs3500/pa05/view/Splash.java
+++ src/main/java/cs3500/pa05/view/Splash.java
@@ -7,6 +7,12 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.util.Duration;
+import javafx.animation.SequentialTransition;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+
 
 /**
  * Opening splash screen
@@ -33,28 +39,40 @@ public class Splash extends StackPane {
    */
   private void initializeSplash() {
     // Set splash screen styling
-    splash.setFont(Font.font("Times New Roman", 40));
-    splash.setTextFill(Color.BLACK);
+    splash.setFont(Font.font("Verdana", 50));
+    splash.setTextFill(Color.WHITE);
+
+    // Add a shadow effect
+    splash.setEffect(new DropShadow(30, Color.BLACK));
+
+    // Set gradient background
+    BackgroundFill backgroundFill = new BackgroundFill(
+        Color.BLUE,
+        new CornerRadii(1),
+        null
+    );
+    Background background = new Background(backgroundFill);
+    this.setBackground(background);
 
     // Create a fade-in transition for the splash screen
-    FadeTransition fadeInTransition = new FadeTransition(Duration.seconds(1), splash);
+    FadeTransition fadeInTransition = new FadeTransition(Duration.seconds(2), splash);
     fadeInTransition.setFromValue(0);
     fadeInTransition.setToValue(1);
 
     // Create a fade-out transition for the splash screen
-    FadeTransition fadeOutTransition = new FadeTransition(Duration.seconds(1), splash);
+    FadeTransition fadeOutTransition = new FadeTransition(Duration.seconds(2), splash);
     fadeOutTransition.setFromValue(1);
     fadeOutTransition.setToValue(0);
-    fadeOutTransition.setDelay(Duration.seconds(1));
+    fadeOutTransition.setDelay(Duration.seconds(2));
 
     // Combine the fade-in and fade-out transitions
-    fadeInTransition.setOnFinished(event -> fadeOutTransition.play());
+    SequentialTransition sequentialTransition = new SequentialTransition(fadeInTransition, fadeOutTransition);
 
     // Add the splash label to the center of the StackPane
     this.getChildren().add(splash);
     StackPane.setAlignment(splash, Pos.CENTER);
 
     // Play the fade-in transition to show the splash screen
-    fadeInTransition.play();
+    sequentialTransition.play();
   }
 }
